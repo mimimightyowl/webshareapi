0a47a9d20a17ac71f531120d5e389f321b9313a5
Tracy Sergeev-Snowskiy <rudi.sergeev@gmail.com>
2022-12-08 01:11:10 +0400

    feat: test api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const shareUrl =
+    "https://adventures.polaris.com/adventure/motorcycle-rental-vancouver-british-columbia/";
+  const handleSmsButtonClick = () => {
+    // eslint-disable-next-line no-undef
+    window.location.href = `sms:?body=Take a ride with me: ${shareUrl}`;
+    const url = window.location.href;
+    navigator.share({ url });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code> src / App.js </code> and save to reload.{" "}
+        </p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React{" "}
+        </a>{" "}
+        <div onClick={() => handleSmsButtonClick()}>Webshare API</div>
+      </header>{" "}
     </div>
   );
 }
